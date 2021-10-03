c67ca9e7df4cb2a184e32510be89273bf8e51773
Moisas <mopri08@gmail.com>
2021-10-03 10:23:25 -0300

    realizado conserto no nome do banco

--- src/connections/db.connection.js
+++ src/connections/db.connection.js
@@ -1,6 +1,6 @@
 import Sequelize from 'sequelize';
 
-const sequelize = new Sequelize("livraria_apinp","root","",
+const sequelize = new Sequelize("livraria_api","root","",
     {
         dialect: "mysql",
         define: {
