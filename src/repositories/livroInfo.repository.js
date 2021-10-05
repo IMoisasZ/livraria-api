82573b6c80b7d264676b38826bcf4c2987d54a92
Moisas <mopri08@gmail.com>
2021-10-05 00:04:16 -0300

    criado os repositorios para inclusão das avliações dos livros, bem como suas alterações e exclusoes

--- src/repositories/livroInfo.repository.js
+++ src/repositories/livroInfo.repository.js
@@ -33,8 +33,46 @@ async function deleteLivroInfo(livro_id){
     }
 }
 
+async function getLivroInfo(livro_id){
+    try {
+        const mongoose = await conn()
+        parseInt(livro_id)
+        const LivroInfo = mongoose.model('LivroInfo', LivroInfoSchema)
+        const livro = LivroInfo.findOne({ livro_id })
+        return await livro.exec()
+    
+    } catch (err) {
+        throw err
+    }
+}
+
+async function createAvaliacao(livro_id, avaliacao){
+    try {
+        const livro = await getLivroInfo(livro_id)
+        livro.avaliacoes.push(avaliacao)
+        await updateLivroInfo(livro)
+        return await livro
+    } catch (err) {
+        throw err
+    }
+}
+
+async function deleteAvaliacao(livro_id, index){
+    try {
+        const livro = await getLivroInfo(livro_id)
+        livro.avaliacoes.splice(index-1, 1)
+        await updateLivroInfo(livro)
+        return await getLivroInfo(livro_id)
+    } catch (err) {
+        throw err
+    }
+}
+
 export default {
     createLivroInfo,
     updateLivroInfo,
-    deleteLivroInfo
+    deleteLivroInfo,
+    getLivroInfo,
+    createAvaliacao,
+    deleteAvaliacao
 }
