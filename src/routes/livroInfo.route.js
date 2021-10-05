b61b8dc4a583c2685bcb367d21d85aab067b971f
Moisas <mopri08@gmail.com>
2021-10-05 00:05:11 -0300

    criado os endpoints para criar, atualizar e deletar as avaliações dos livros

--- src/routes/livroInfo.route.js
+++ src/routes/livroInfo.route.js
@@ -5,6 +5,11 @@ const router = express.Router()
 
 router.post('/',LivroInfoController.createLivroInfo)
 router.put('/',LivroInfoController.updateLivroInfo)
+router.get('/:livro_id', LivroInfoController.getLivroInfo)
 router.delete('/:livro_id',LivroInfoController.deleteLivroInfo)
+
+// avaliação
+router.post('/:livro_id/avaliacao', LivroInfoController.createAvaliacao)
+router.delete('/:livro_id/avaliacao/:index', LivroInfoController.deleteAvaliacao)
 
 export default router
