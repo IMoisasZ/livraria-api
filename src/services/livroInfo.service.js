f2a969fcd750bd8d07c3cbf2ce2cf9a0ace329f0
Moisas <mopri08@gmail.com>
2021-10-05 00:06:05 -0300

    criado os serviços com as regras de negocio da aplicação para as avaliações dos livros

--- src/services/livroInfo.service.js
+++ src/services/livroInfo.service.js
@@ -1,3 +1,4 @@
+import livroInfoRepository from '../repositories/livroInfo.repository.js'
 import LivroInfoRepository from '../repositories/livroInfo.repository.js'
 
 async function createLivroInfo(livroInfo){
@@ -12,8 +13,23 @@ async function deleteLivroInfo(livro_id){
     await LivroInfoRepository.deleteLivroInfo(livro_id)
 }
 
+async function getLivroInfo(livro_id){
+    return await LivroInfoRepository.getLivroInfo(livro_id)
+}
+
+async function createAvaliacao(livro_id, avaliacao){
+    return await LivroInfoRepository.createAvaliacao(livro_id, avaliacao)
+}
+
+async function deleteAvaliacao(livro_id, index){
+    return await LivroInfoRepository.deleteAvaliacao(livro_id, index)
+}
+
 export default {
     createLivroInfo,
     updateLivroInfo,
-    deleteLivroInfo
+    deleteLivroInfo,
+    getLivroInfo,
+    createAvaliacao,
+    deleteAvaliacao
 }
