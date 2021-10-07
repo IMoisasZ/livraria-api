3497be8d13e4763946ed20e1cf217fce517eae49
Moisas <mopri08@gmail.com>
2021-10-06 23:40:02 -0300

    acrescentado a rota os middleware que vão fazer a verificação das rotas publicas e privadas

--- src/routes/livroInfo.route.js
+++ src/routes/livroInfo.route.js
@@ -1,15 +1,17 @@
 import express from 'express'
 import LivroInfoController from '../controllers/livroInfo.controller.js'
+import authPrivate from '../middlewares/authPrivate.js'
+import authPublic from '../middlewares/authPublic.js'
 
 const router = express.Router()
 
-router.post('/',LivroInfoController.createLivroInfo)
-router.put('/',LivroInfoController.updateLivroInfo)
-router.get('/:livro_id', LivroInfoController.getLivroInfo)
-router.delete('/:livro_id',LivroInfoController.deleteLivroInfo)
+router.post('/', authPrivate, LivroInfoController.createLivroInfo)
+router.put('/', authPrivate, LivroInfoController.updateLivroInfo)
+router.get('/:livro_id', authPrivate, LivroInfoController.getLivroInfo)
+router.delete('/:livro_id', authPrivate, LivroInfoController.deleteLivroInfo)
 
 // avaliação
-router.post('/:livro_id/avaliacao', LivroInfoController.createAvaliacao)
-router.delete('/:livro_id/avaliacao/:index', LivroInfoController.deleteAvaliacao)
+router.post('/:livro_id/avaliacao', authPublic, LivroInfoController.createAvaliacao)
+router.delete('/:livro_id/avaliacao/:index', authPrivate, LivroInfoController.deleteAvaliacao)
 
 export default router
