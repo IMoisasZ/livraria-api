69cedae92ba93f7fb589f82b1475be1acb5fbd25
Moisas <mopri08@gmail.com>
2021-10-08 01:36:09 -0300

    criado um teste para o endpoint do /autor

--- test/autor.test.js
+++ test/autor.test.js
@@ -1,25 +1,7 @@
-import request from 'supertest'
-import app from '../index.js'
+import supertest from 'supertest';
 
-describe('Testes de autor', ()=>{
-    test.only('Criar um autor com dados de teste.', async ()=>{
-        const autor = {
-            nome: 'Pereira',
-            email: 'pereira@email.com',
-            telefone: '12345678'
-        }
-        
-       const res = await request(app)
-      .post('/autor')
-      .send(autor)
-        
-      expect(res.status).toBe(201)
-
-      const autorCadastrado = await request(app)
-      .get('/autor/:autor_id')
-      .send(res.autor_id)
-        console.log(res);
-      expect(autorCadastrado.email).toBe(autor.email)
-
+const request = supertest('http://localhost:3000')
+    test('Criar um autor com dados de teste.', async ()=>{        
+       const resposta = await request.get('/autor').auth('admin','desafio-igti-nodejs')
+      expect(resposta.status).toBe(200)
     })
-})
