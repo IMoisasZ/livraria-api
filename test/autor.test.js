97b62eee78ff211ea53169402bbece95e3aa2266
Moisas <mopri08@gmail.com>
2021-10-08 12:46:18 -0300

    merge

--- test/autor.test.js
+++ test/autor.test.js
@@ -2,6 +2,6 @@ import supertest from 'supertest';
 
 const request = supertest('http://localhost:3000')
     test('Criar um autor com dados de teste.', async ()=>{        
-       const resposta = await request.get('/autor').auth('admin','desafio-igti-nodejs')
+       const resposta = await request.get('/autor')
       expect(resposta.status).toBe(200)
     })
