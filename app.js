a7c9ce13798a88a467455e4238c0b8144e9c3ce3
Moisas <mopri08@gmail.com>
2021-10-08 01:37:15 -0300

    confrome solicitado pelo documentação do supertest, foi alterado o index.js retirando o servidor e alterado o nome para app.js

---
+++ app.js
@@ -0,0 +1,46 @@
+import express from 'express'
+import winston from 'winston'
+import ClienteRoute from './src/routes/cliente.route.js'
+import VendaRoute from './src/routes/venda.route.js'
+import AutorRoute from './src/routes/autor.route.js'
+import LivroRoute from './src/routes/livro.route.js'
+import LivroInfoRoute from './src/routes/livroInfo.route.js'
+
+const app = express()
+const port = 3000
+
+app.use(express.json())
+
+// winston(log)
+const { combine, timestamp, label, printf} = winston.format;
+const myformat = printf (({ level, message, label, timestamp}) => {
+    return `${timestamp} [${label}] ${level} ${message}`;
+})
+global.logger = winston.createLogger({
+    level: "silly",
+    transports: [
+        new (winston.transports.Console)(),
+        new (winston.transports.File)({filename: "livraria-log" })
+    ],
+    format: combine(
+        label({ label: "livraria-api" }),
+        timestamp(),
+        myformat
+    )
+});
+
+// rotas
+app.use('/cliente', ClienteRoute)
+app.use('/venda', VendaRoute)
+app.use('/autor', AutorRoute)
+app.use('/livro', LivroRoute)
+app.use('/livroInfo', LivroInfoRoute)
+
+// erro padrão
+app.use((err, req, res, next) => {
+    logger.error(`${req.method} ${req.baseUrl} - ${err.message}`)
+    res.status(400).send({ erros: err.message })
+})
+
+export default app
+
